b1dee5f5f04714e3ba5311235dbf015a5dbc2001
reinhardbuyabo <102545449+reinhardbuyabo@users.noreply.github.com>
2023-08-28 15:06:14 +0000

    Trial 2

--- index.js
+++ index.js
@@ -18,13 +18,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalid = (date) => date.toUTCString === 'Invalid Date'
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  if (isInvalid(date)){
+    date = new Date(parseInt(req.params.date))
+  }
+
+  if (isInvalid(date)){
+    res.json({
+      error: 'Invalid Date'
+    })
+    return
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()    
+  })
 });
 
-
+app.get("/api", function(req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
